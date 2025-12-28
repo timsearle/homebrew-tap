7581daeda16e8ddd75449c425438dfb2c228bf30
Tim Searle <tim@searle.dev>
2025-12-28 23:28:50 +0000

    swift-dependency-graph 0.7.2

--- Formula/swift-dependency-graph.rb
+++ Formula/swift-dependency-graph.rb
@@ -1,9 +1,9 @@
 class SwiftDependencyGraph < Formula
   desc "Dependency graph generator for Xcode + SwiftPM projects"
   homepage "https://github.com/timsearle/swift-dependency-graph"
-  url "https://github.com/timsearle/swift-dependency-graph/releases/download/v0.1.0/DependencyGraph-macos-arm64.zip"
-  sha256 "070b6875b01cfd1878a6ddcac8d76eba6078b29e179a58eb92cd277b446e542a"
-  version "0.1.0"
+  url "https://github.com/timsearle/swift-dependency-graph/releases/download/v0.7.2/DependencyGraph-macos-arm64.zip"
+  sha256 "5fb9fdca70cc3c1c275e836b96f818d5b89793953fb4a89f26330908ea3b815c"
+  version "0.7.2"
   license "MIT"
 
   depends_on arch: :arm64
